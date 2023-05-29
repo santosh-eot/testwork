a3f2216669ef851b4a2e77dbbadb42addf2f1d62
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-05-29 18:25:14 +0530

    temp

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const bodyParser = require('body-parser')
+
 AWS.config.update({
     region: process.env.REGION,
     accessKeyId: process.env.ACCESS_KEY_ID,
@@ -14,34 +16,41 @@ let sendQueueURL = process.env.SEND_QUEUE_URL;
 
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
 io.on('connection', (socket) => { 
     // console.log('socket connected')
     socket.on('recive message', async(data)=>{
         try {
-            console.log(`message recived ${data.message}`)
-            const params = {
-                MessageBody: data.message,
-                QueueUrl: sendQueueURL
-            };
+            // console.log(`message recived ${data.message}`)
+            // const params = {
+            //     MessageBody: data.message,
+            //     QueueUrl: sendQueueURL
+            // };
               
-            let result = await sqs.sendMessage(params).promise();
-            console.log('Result', result)
-            socket.emit('print', {message: `send message to sqs : ${data.message}`})
+            // let result = await sqs.sendMessage(params).promise();
+            // console.log('Result', result)
+            socket.emit('print', {message: `send message to sqs1 : ${data.message}`})
         } catch (error) {
             console.log(error)
         }
     })
     socket.on('recived message lambda', async(data)=>{
         console.log('message recive from sqs', data)
-        socket.emit('print', {message: `recived message from lambda : ${data.message}`})
+        
     }) 
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + "/index1.html");
+    });
+    
+    app.post('/get-message', (req, res)=>{
+        socket.emit('print', {message: `recived message from sqs2 : ${req.body.message}`})
+    })
 });
 
 server.listen(3000, ()=>console.log('server running at port 3000'));
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-    console.log('hello')
-    res.sendFile(__dirname + "/index1.html");
-});
+
